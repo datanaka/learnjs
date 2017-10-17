37a03f5a51d386b9b65e993e42ce716061d020fd
datanaka <daiki.a.tanaka@gmail.com>
2017-10-17 22:42:15 +0900

    developed function of data binding

--- public/app.js
+++ public/app.js
@@ -2,9 +2,22 @@
 
 var learnjs = {};
 
-learnjs.problemView = function(problemNumber) {
+learnjs.problems = [
+	{
+		description: "What is truth?",
+		code: "function problem() { return __; }"
+	},
+	{
+		description: "Simple Math",
+		code: "function problem() { return 42 === 6 * __; }"
+	}
+];
+
+learnjs.problemView = function(data) {
+	var problemNumber = parseInt(data, 10);
 	var view = $('.templates .problem-view').clone();
 	view.find('.title').text('Problem #' + problemNumber);
+	learnjs.applyObject(learnjs.problems[problemNumber - 1], view);
 	return view;
 }
 
@@ -25,3 +38,9 @@ learnjs.appOnReady = function() {
 	};
 	learnjs.showView(window.location.hash);
 }
+
+learnjs.applyObject = function(obj, elem) {
+	for (var key in obj) {
+		elem.find('[data-name="' + key + '"]').text(obj[key]);
+	}
+};
