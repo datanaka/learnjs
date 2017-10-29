23d185204edd83c435be9d058611a2b5e7627a3d
datanaka <daiki.a.tanaka@gmail.com>
2017-10-29 21:24:33 +0900

    解答結果のフェードイン・アウト処理追加

--- public/app.js
+++ public/app.js
@@ -27,9 +27,9 @@ learnjs.problemView = function(data) {
 
 	function checkAnswerClick() {
 		if(checkAnswer()) {
-			resultFlash.text('Correct!');
+			learnjs.flashElement(resultFlash, 'Correct!');
 		} else {
-			resultFlash.text('Incorrect!');
+			learnjs.flashElement(resultFlash, 'Incorrect!');
 		}
 		return false;
 	}
@@ -63,3 +63,10 @@ learnjs.applyObject = function(obj, elem) {
 		elem.find('[data-name="' + key + '"]').text(obj[key]);
 	}
 };
+
+learnjs.flashElement = function(elem, content) {
+	elem.fadeOut('fast', function() {
+		elem.html(content);
+		elem.fadeIn();
+	});
+}
